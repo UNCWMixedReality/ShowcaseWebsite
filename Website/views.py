3c864647e31114defb85affbfd2ac957298367fa
Kegeris <dk9144@uncw.edu>
2022-06-27 11:48:56 -0400

    First push for project website

---
+++ Website/views.py
@@ -0,0 +1,53 @@
+from flask import Blueprint, render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+@views.route('/VASCProject')
+def Vasc():
+    return render_template("VASC Project.html", text="Testing", user ="Testing2", boolean = True)
+
+@views.route('/About')
+def about():
+    return render_template("About.html")
+
+@views.route('/OtherProjects')
+def OtherProj():
+    return render_template("OtherProjects.html")
+
+@views.route('/Publ')
+def Publ():
+    return render_template("Publ.html")
+
+@views.route('/REU')
+def REU():
+    return render_template("REU.html")
+
+@views.route('/VASCHome')
+def VASCHome():
+    return render_template("homeVASC.html")
+
+@views.route('/TeacherPortal')
+def TeacherPortal():
+    return render_template("TeacherPortal.html")
+
+@views.route('/VASCPubl')
+def VASCPubl():
+    return render_template("PublVASC.html")
+
+@views.route('/VASCAbout')
+def VASCAbout():
+    return render_template("AboutVASC.html")
+
+@views.route('/VASCContributors')
+def VASCContributors():
+    return render_template("ContributorsVASC.html")
+
+@views.route('/VASCPhotoGallery')
+def VASCPhotoGallery():
+    return render_template("PhotoGalleryVASC.html")
+
+
